9819cd22c15b60349d84c929b538caa85d5e49f8
Nguyen-Thi-Xuan-Hoa <20130038@st.hcmuaf.edu.vn>
2022-11-27 17:09:07 +0700

    commit

---
+++ Navicat/MySQL/Servers/LTWEB/projectltw/ltWeb.sql
@@ -0,0 +1,9 @@
+INSERT INTO menu VALUES('m1','Gạo');
+INSERT INTO menu VALUES('m3','Các loại Nếp');
+INSERT INTO menu VALUES('m2','Các loại bột');
+INSERT INTO menu VALUES('m4','Các loại hạt');
+INSERT INTO menu VALUES('m4','Các loại củ, trái');
+
+INSERT INTO product VALUES()
+
+INSERT INTO image VALUES('img1','
