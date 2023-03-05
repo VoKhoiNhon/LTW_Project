7e9ecaa2c25a95b2de20f534ceb347b5f15eefaf
Võ Khôi Nhơn <20130355@st.hcmuaf.edu.vn>
2023-03-05 18:06:07 +0700

    update log

--- main_project/BHNFoods/src/main/java/vn/edu/hcmuaf/fit/controller/AddToLoveProd.java
+++ main_project/BHNFoods/src/main/java/vn/edu/hcmuaf/fit/controller/AddToLoveProd.java
@@ -1,6 +1,8 @@
 package vn.edu.hcmuaf.fit.controller;
 
+import vn.edu.hcmuaf.fit.beans.Log;
 import vn.edu.hcmuaf.fit.beans.User;
+import vn.edu.hcmuaf.fit.db.DB;
 import vn.edu.hcmuaf.fit.service.CartService;
 import vn.edu.hcmuaf.fit.service.LoveProdService;
 
@@ -11,6 +13,7 @@ import java.io.IOException;
 
 @WebServlet(name = "AddToLoveProd", value = "/addToLoveProd")
 public class AddToLoveProd extends HttpServlet {
+    String src ="love Product";
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String idProd = request.getParameter("id");
@@ -20,9 +23,16 @@ public class AddToLoveProd extends HttpServlet {
         int condition = Integer.parseInt(request.getParameter("condition"));
         if(condition == 0) {
             LoveProdService.getInstance().deleteFromLike(idUser,idProd);
+            if (idUser == null) {
+                DB.me().insert(new Log(Log.INFO, null, this.src, "remove lovePr : " +idProd, 0));
+            } else DB.me().insert(new Log(Log.INFO, idUser, this.src,  "remove lovePr: "+idProd, 0));
         }else {
             LoveProdService.getInstance().insertToLike(idUser,idProd);
+            if (idUser == null) {
+                DB.me().insert(new Log(Log.INFO, null, this.src, "add lovePr : " +idProd, 0));
+            } else DB.me().insert(new Log(Log.INFO, idUser, this.src,  "add lovePr: "+idProd, 0));
         }
+
     }
 
     @Override
