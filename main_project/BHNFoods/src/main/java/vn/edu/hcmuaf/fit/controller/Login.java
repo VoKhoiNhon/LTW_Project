3b8b67fa29fbc7539d996024c0bd73e966466b34
Võ Khôi Nhơn <20130355@st.hcmuaf.edu.vn>
2023-03-06 02:57:39 +0700

    fix lỗi merge ở login =((

--- main_project/BHNFoods/src/main/java/vn/edu/hcmuaf/fit/controller/Login.java
+++ main_project/BHNFoods/src/main/java/vn/edu/hcmuaf/fit/controller/Login.java
@@ -1,6 +1,8 @@
 package vn.edu.hcmuaf.fit.controller;
 
+import vn.edu.hcmuaf.fit.beans.Log;
 import vn.edu.hcmuaf.fit.beans.User;
+import vn.edu.hcmuaf.fit.db.DB;
 import vn.edu.hcmuaf.fit.service.UserService;
 import vn.edu.hcmuaf.fit.util.Brower;
 import vn.edu.hcmuaf.fit.util.Encryption;
@@ -19,11 +21,10 @@ import java.util.List;
 
 @WebServlet(name = "Login", value = "/Login")
 public class Login extends HttpServlet {
-<<<<<<< Updated upstream
-=======
+
     String src = "LOGIN";
 
->>>>>>> Stashed changes
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("login.jsp").forward(request, response);
@@ -40,10 +41,9 @@ public class Login extends HttpServlet {
         if (user != null && user.getDecentralization() == 1) {
             HttpSession session = request.getSession(true);
             session.setAttribute("auth", user);
-<<<<<<< Updated upstream
-=======
+
             DB.me().insert(new Log(Log.INFO, user.getIdUser(), this.src, "LOGIN SUCCESS", 0, Brower.getBrowerName(request.getHeader("User-Agent")),Brower.getLocationIp(request.getRemoteAddr())));
->>>>>>> Stashed changes
+
             response.sendRedirect("AdminMain");
 
         } else if (user != null && user.getDecentralization() != 1) {
@@ -51,17 +51,13 @@ public class Login extends HttpServlet {
             session.setAttribute("auth", user);
             session.setAttribute("idUser", user.getIdUser());
             request.setAttribute("idUser", user.getIdUser());
-<<<<<<< Updated upstream
-            response.sendRedirect("http://localhost:8080/BHNFoods/index?idUser=" + user.getIdUser());
-        } else {
-            request.setAttribute("error", "Sai tài khoản hoặc mật khẩu");
-=======
+
             DB.me().insert(new Log(Log.INFO, user.getIdUser(), this.src, "LOGIN SUCCESS", 0, Brower.getBrowerName(request.getHeader("User-Agent")),Brower.getLocationIp(request.getRemoteAddr())));
             response.sendRedirect("http://localhost:8080/BHNFoods/index?idUser=" + user.getIdUser());
         } else {
             request.setAttribute("error", "Sai tài khoản hoặc mật khẩu");
             DB.me().insert(new Log(Log.WARNING, null, this.src, "LOGIN FALSE: " + username, 0, Brower.getBrowerName(request.getHeader("User-Agent")),Brower.getLocationIp(request.getRemoteAddr())));
->>>>>>> Stashed changes
+
             request.getRequestDispatcher("login.jsp").forward(request, response);
         }
 
