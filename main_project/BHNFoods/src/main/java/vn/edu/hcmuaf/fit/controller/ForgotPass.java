b4d62886db62550df37adbc88368cf19dfeca7ed
Pham-Gia-Bao <113536358+Pham-Gia-Bao@users.noreply.github.com>
2023-03-07 15:11:53 +0700

    Chức năng verifyEmail cho quên mật khẩu

--- main_project/BHNFoods/src/main/java/vn/edu/hcmuaf/fit/controller/ForgotPass.java
+++ main_project/BHNFoods/src/main/java/vn/edu/hcmuaf/fit/controller/ForgotPass.java
@@ -2,7 +2,9 @@ package vn.edu.hcmuaf.fit.controller;
 
 import vn.edu.hcmuaf.fit.beans.User;
 import vn.edu.hcmuaf.fit.service.UserService;
+import vn.edu.hcmuaf.fit.service.VerifyingService;
 import vn.edu.hcmuaf.fit.util.Encryption;
+import vn.edu.hcmuaf.fit.util.MailSender;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -20,25 +22,42 @@ public class ForgotPass extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String account = request.getParameter("checkUserName");
-        String pass = request.getParameter("pass");
-        String repass = request.getParameter("repass");
-        String code = request.getParameter("code");
 
-        request.setAttribute("mess", codeRandom);
+//        String pass = request.getParameter("pass");
+//        String repass = request.getParameter("repass");
+//        String code = request.getParameter("code");
+
 
         if (!UserService.getInstance().checkAccountExist(account, account)) {
             request.setAttribute("errorName", "* Tài khoản ko tồn tại");
             request.getRequestDispatcher("changepass.jsp").forward(request, response);
-        } else if (!codeRandom.equals(code)) {
-            request.setAttribute("errorCode", "* Mã xác nhận ko trùng khớp");
+        } else {
+            HttpSession session = request.getSession();
+            User user = UserService.getInstance().getUserByPhoneOrEmail(account);
+            String salt = Encryption.randomSalt();
+            String key = Encryption.toSHA1(user.getEmail() + salt);
+            session.setAttribute("idUser", user.getIdUser());
+            VerifyingService.getInstance().addKey(key, salt);
+            String mailMessage = "<!DOCTYPE html>\n" +
+                    "<html lang=\"en\">\n" +
+                    "<head>\n" +
+                    "  <meta charset=\"UTF-8\">\n" +
+                    "  <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n" +
+                    "  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
+                    "  <title>Document</title>\n" +
+                    "</head>\n" +
+                    "<body>\n" +
+                    "  <a style=\"text-decoration: none; padding: 10px 20px; color: black; background:rgb(192, 192, 241);\" href='http://localhost:8080/BHNFoods/verifyingEmail?key="+key+"&different="+salt+"'>Xác thực ngay</a>\n" +
+                    "</body>\n" +
+                    "</html>";
+            MailSender.send("Xác thực thông tin", mailMessage, user.getEmail());
+            StringBuilder sb = new StringBuilder();
+            for (int i = user.getEmail().length() - 4; i > 0; i--) {
+                sb.append("*");
+            }
+            String hideMail = user.getEmail().substring(0, 4) + sb.toString();
+            request.setAttribute("mailSent", "Thư đã được gửi vào email " + hideMail);
             request.getRequestDispatcher("changepass.jsp").forward(request, response);
-        } else if (!pass.equals(repass)) {
-            request.setAttribute("errorDup", "* Mật khẩu không trùng khớp");
-            request.getRequestDispatcher("changepass.jsp").forward(request, response);
-        } else if (pass.equals(repass)) {
-            pass = Encryption.toSHA1(pass);
-            UserService.getInstance().changePass(account, account, pass);
-            request.getRequestDispatcher("login.jsp").forward(request, response);
         }
     }
 }
